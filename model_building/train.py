b22bef4b3f7cf394a3979ecf9463f49bd2901bc1
singh-payal <108542539+singh-payal@users.noreply.github.com>
2026-06-04 21:25:56 +0530

    Change MLflow tracking URI to localhost

--- model_building/train.py
+++ model_building/train.py
@@ -20,11 +20,8 @@ from huggingface_hub.utils import RepositoryNotFoundError, HfHubHTTPError
 import mlflow
 
 # Set up MLflow tracking (using file-based URI for GitHub Actions)
-mlruns_path = os.path.abspath("mlruns")
-mlflow.set_tracking_uri(f"file://{mlruns_path}")
+mlflow.set_tracking_uri("http://localhost:5000")
 mlflow.set_experiment("4th-tourism-package-prediction")
-api = HfApi(token=os.getenv("HF_TOKEN")) # Initialize with token
-repo = "singhpayal/4th_tourism_project_prediction_dataset"
 
 # Load the preprocessed data from Hugging Face
 print("Loading preprocessed data...")
